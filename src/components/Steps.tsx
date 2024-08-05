d25bc3952a45d753c718cebd783e45ec744cc8f0
Foks9n <foks1928@yandex.ru>
2024-08-05 22:04:53 +0300

    rework edit data

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,81 +1,139 @@
-import React, { useState } from 'react';
-import './Steps.css';
+import { useState } from "react";
+import "./Steps.css";
 
-interface DistanceData {
+interface IRecordsData {
   date: string;
   distance: number;
 }
 
-const Steps: React.FC = () => {
-  const [distances, setDistances] = useState<DistanceData[]>([]);
-  const [newDate, setNewDate] = useState<string>('');
-  const [newDistance, setNewDistance] = useState<number | ''>('');
+export default function Steps() {
+  const [records, setRecords] = useState<IRecordsData[]>([]);
+  const [date, setDate] = useState<string>("");
+  const [distance, setDistance] = useState<number | "">("");
+  const [editIndex, setEditIndex] = useState<number>(-1);
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (date && distance) {
+      const newRecords = [...records];
+      const foundRecord = records.find((item) => item.date === date);
+
+      if (foundRecord) {
+        foundRecord.distance += Number(distance);
+        setRecords(newRecords);
+      } else {
+        newRecords.push({ date, distance: Number(distance) });
+      }
+      newRecords.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      setRecords(newRecords);
+      setDate("");
+      setDistance("");
+      setEditIndex(-1);
+    }
+  };
 
   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewDate(event.target.value);
+    setDate(event.target.value);
   };
 
   const handleDistanceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewDistance(event.target.value as number | '');
+    setDistance(event.target.value as number | "");
   };
 
-  const handleAddDistance = () => {
-    if (newDate && newDistance !== '') {
-      const existingIndex = distances.findIndex((item) => item.date === newDate);
+  const saveEditedRecord = (event: React.FormEvent) => {
+    event.preventDefault();
 
-      if (existingIndex !== -1) {
-        const updatedDistances = [...distances];
-        updatedDistances[existingIndex].distance += Number(newDistance);
-        setDistances(updatedDistances);
-      } else {
-        setDistances([...distances, { date: newDate, distance: Number(newDistance) }]);
-      }
+    if (editIndex >= 0) {
+      const newRecords = [...records];
 
-      setNewDate('');
-      setNewDistance('');
+      newRecords[editIndex] = { date, distance: Number(distance) };
+      newRecords.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      setRecords(newRecords);
+      setDate("");
+      setDistance("");
+      setEditIndex(-1);
     }
   };
 
-  const handleRemoveDistance = (index: number) => {
-    const updatedDistances = distances.filter((_, i) => i !== index);
-    setDistances(updatedDistances);
+  const handleEditRecord = (index: number) => {
+    const recToEdit = records[index];
+    setDate(recToEdit.date);
+    setDistance(recToEdit.distance);
+    setEditIndex(index);
   };
 
-  const sortedDistances = [...distances].sort((a, b) => {
-    return new Date(b.date.split('.').reverse().join('-')).getTime() - new Date(a.date.split('.').reverse().join('-')).getTime();
-  });
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      handleAddDistance();
-    }
+  const handleRemoveRecord = (index: number) => {
+    const newRecords = records.filter((_, i) => i !== index);
+    setRecords(newRecords);
   };
 
   return (
     <>
-      <div className='title'>
-        <h4>Дата (ДД.ММ.ГГГГ)</h4>
-        <h4>Пройдено км</h4>
-      </div>
-      <form className='input'>
-        <input type="text" value={newDate} onChange={handleDateChange} onKeyDown={handleKeyDown} />
-        <input type="number" step="0.1" value={newDistance} onChange={handleDistanceChange} onKeyDown={handleKeyDown} />
-        <button type="button" onClick={handleAddDistance}>Добавить</button>
-      </form>
-      <div className='title-result'>
-        <p style={{ paddingRight: '10px' }}>Дата (ДД.ММ.ГГГГ)</p><p style={{ paddingRight: '10px' }}>Пройдено км</p><p style={{ paddingRight: '10px' }}>Действия</p>
-      </div>
-      <div className='output'>
-        {sortedDistances.map((item, index) => (
-          <div key={index}>
-            <span style={{ marginRight: '70px' }}>{item.date}</span>
-            <span style={{ marginRight: '70px' }}>{item.distance}</span>
-            <span onClick={() => handleRemoveDistance(index)} style={{ cursor: 'pointer' }}>✎ ✘</span>
+      <div className="inputs">
+        <form className="tr" onSubmit={onSubmit}>
+          <div className="inputs-headers">
+            <h4 className="date-header">Дата</h4>
+            <h4 className="km-header">Пройдено км</h4>
           </div>
-        ))}
+
+          <input
+            className="date-input"
+            type="date"
+            value={date}
+            onChange={handleDateChange}
+          />
+
+          <input
+            className="km-input"
+            type="number"
+            placeholder="Километры"
+            step={0.1}
+            min={0}
+            value={distance}
+            onChange={handleDistanceChange}
+          />
+
+          {editIndex === -1 ? (
+            <button type="submit" className="td">
+              Добавить
+            </button>
+          ) : (
+            <button onClick={saveEditedRecord} className="td">
+              Ok
+            </button>
+          )}
+        </form>
       </div>
+      <table style={{ marginTop: "50px" }} width={550}>
+        <thead>
+          <tr className="records-headers">
+            <th scope="col">Дата</th>
+            <th scope="col">Пройдено км</th>
+            <th scope="col" colSpan={2}>
+              Действия
+            </th>
+          </tr>
+        </thead>
+        <tbody className="records">
+          {records.map((item, index) => (
+            <tr key={index}>
+              <td scope="row">{item.date.split("-").reverse().join(".")}</td>
+              <td scope="row">{item.distance}</td>
+              <td scope="row" onClick={() => handleEditRecord(index)}>
+                ✎
+              </td>
+              <td scope="row" onClick={() => handleRemoveRecord(index)}>
+                ✘
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
-};
-
-export default Steps;
+}
